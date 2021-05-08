e95603d190c8e891fa04f5c0034bf32852592e83
4923 <202003774y@gmail.com>
2021-05-08 22:56:41 +0900

    Update : subject of activation mail Stackoberflow to HufsOverflow 기본 설정인 것 같아 서비스명 변경

--- stackoverflow/userauth/views.py
+++ stackoverflow/userauth/views.py
@@ -26,7 +26,7 @@ def signup(request):
             user.save()
 
             current_site = get_current_site(request)
-            mail_subject = "Activate your Stackoberflow account."
+            mail_subject = "Activate your HufsOverflow account."
             message = render_to_string(
                 "userauth/user_active_email.html",
                 {
