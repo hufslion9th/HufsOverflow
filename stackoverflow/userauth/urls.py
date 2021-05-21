54c6613bd3d7b66943d3242bdcdcebe40411d877
SeolHuiGwan9478 <shg5977@naver.com>
2021-05-22 01:25:02 +0900

    implementation find password func

--- stackoverflow/userauth/urls.py
+++ stackoverflow/userauth/urls.py
@@ -15,10 +15,14 @@ urlpatterns = [
     path("logout/", views.logout_request, name="name_logout_req"),
     path("editprofile/", views.editprofile, name="name_editprofile"),
     url(
-        r"^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$",
+        r"^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,32})/$",
         views.activate,
         name="activate",
     ),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="userauth/password_reset.html"), name='reset_password'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='userauth/password_reset_done.html'), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="userauth/password_reset_confirm.html"), name="password_reset_confirm"),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="userauth/password_reset_complete.html"), name="password_reset_complete"),
 ]
 
 if settings.DEBUG:
